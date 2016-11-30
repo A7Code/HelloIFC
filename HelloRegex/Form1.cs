7d18a869be81d135bd24081bf15aab702713abe9
ZhongQi_Outside <zhongqi1105@gmail.com>
2016-11-30 16:48:37 +0800

    Cut IFC file by Regex

---
+++ HelloRegex/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace HelloRegex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                textBox1.Text = @"C:\Users\user\Desktop\theBuilding.ifc";
+
+            StreamReader reader = new StreamReader(textBox1.Text);
+            MatchCollection getMatches = Regex.Matches(reader.ReadToEnd(), "#.*?;");//Wow 威猛
+            richTextBox1.Text = "Count : " + getMatches.Count + "\n";
+            reader.Close();
+
+            StringBuilder build = new StringBuilder("");
+
+            for (int i = 0; i < getMatches.Count; i++)
+                build.Append(getMatches[i] + "\n");
+
+
+            StreamWriter writer = new StreamWriter(textBox1.Text + ".test");
+            writer.Write(build);
+            writer.Close();
+        }
+    }
+}
---
+++ HelloRegex/a7Message.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HelloRegex
+{
+    class a7Message
+    {
+        string _full;
+        List<Match> _find = new List<Match>();
+        public a7Message(ref string _inputString)
+        {
+            _full = _inputString;
+        }
+
+        public void setRegex(string _set)
+        {
+            _find = new List<Match>();
+            MatchCollection getMatches = Regex.Matches(_full, _set);
+            for (int i = 0; i < getMatches.Count; i++)
+                _find.Add(getMatches[i]);
+        }
+
+        public void showSomething()
+        {
+            Console.WriteLine("Count : " + _find.Count);
+            Console.WriteLine("First : " + _find[0]);
+            Console.WriteLine("Last : " + _find.Last());
+        }
+        public void showCount()
+        {
+            Console.WriteLine(_find.Count);
+        }
+        public void showMessage()
+        {
+           for (int i = 0; i < _find.Count; i++)
+                Console.WriteLine(_find[i]);
+            Console.WriteLine();
+        }
+    }
+}
